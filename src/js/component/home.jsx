6816de75905d63dae73c2251c700d207c9d21164
Carolth7 <carol.cth93@gmail.com>
2021-10-31 20:42:00 +0000

    fourCounter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,27 +3,98 @@ import Counter from "./Counter.jsx";
 
 const Home = () => {
 	return (
-		<>
-			<h1>Counter</h1>
-			<div className="containerWhite">
-				<div className="bordercontainer">
-					<div className="container">
-						<span className="seconds">
-							<Counter start={0} time={100000} />
-						</span>
-						:
-						<div className="minutes">
-							<Counter start={0} time={10000} />
+		<Fragment>
+			<div className="row">
+				<div className="col-6">
+					<div className="leftOn">
+						<div className="containerOnLeft">
+							<div className="bordercontainer">
+								<div className="container">
+									<span className="seconds">
+										<Counter start={0} time={50000} />
+									</span>
+									:
+									<div className="minutes">
+										<Counter start={0} time={5000} />
+									</div>
+									:
+									<div className="minutes">
+										<Counter start={0} time={500} />
+									</div>
+								</div>
+							</div>
 						</div>
-						:
-						<div className="minutes">
-							<Counter start={0} time={1000} />
+					</div>
+				</div>
+
+				<div className="col-6">
+					<div className="leftRight">
+						<div className="containerOnRight">
+							<div className="bordercontainer">
+								<div className="container">
+									<span className="seconds">
+										<Counter start={0} time={1000} />
+									</span>
+									:
+									<div className="minutes">
+										<Counter start={0} time={100} />
+									</div>
+									:
+									<div className="minutes">
+										<Counter start={0} time={10} />
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<h2>&#x1f64c; Carolth7</h2>
-		</>
+			<div className="row">
+				<div className="col-6">
+					<div className="leftDown">
+						<div className="containerDownLeft">
+							<div className="bordercontainer">
+								<div className="container">
+									<span className="seconds">
+										<Counter start={0} time={10000} />
+									</span>
+									:
+									<div className="minutes">
+										<Counter start={0} time={1000} />
+									</div>
+									:
+									<div className="minutes">
+										<Counter start={0} time={100} />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="col-6">
+					<div className="rightDown">
+						<div className="containerDownRight">
+							<div className="bordercontainer">
+								<div className="container">
+									<span className="seconds">
+										<Counter start={0} time={100000} />
+									</span>
+									:
+									<div className="minutes">
+										<Counter start={0} time={10000} />
+									</div>
+									:
+									<div className="minutes">
+										<Counter start={0} time={1000} />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
 	);
 };
 
